60b8b8a93c62935463d07a1ac95cb34e0252cabd
freshcodeAlice <freshjs.ofte@gmail.com>
2022-11-03 14:45:19 +0200

    Whos first?

--- index.js
+++ index.js
@@ -1,36 +1,18 @@
 
-/// Promise
-/*
-Створення промісу
-
-const promise = new Promise(executor);
-
-executor = function
-
-*/
-
 
 const promise = new Promise(function(resolve, reject){
-      if(Math.random() > 0.5) {
-        resolve('It`s okay');
-      } else {
-        reject('It`s not okay');
-      }
+    resolve('Promise RESOLVE');
 });
 
-console.log(promise);
+setTimeout(function(){
+    console.log('tick')
+},0);
 
 promise.then((string) => {
-    console.log('RESOLVE: ', string);
+    console.log(string);
+    return Promise.resolve('New Promise resolve')
 }, (error) => {
     console.log('REJECT: ', error);
-});
-
-
-/*
-Створіть новий проміс, який буде викликати resolve, якщо 2+2 === 4
-і reject,якщо ні.
-Навісити обробники через then, які виведуть на консоль результат
-
-
-*/
+}).then((value) => {
+    console.log(value);
+})
