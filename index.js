a4ba33986ab07025981d0e4f05f9c4c206d93c47
freshcodeAlice <freshjs.ofte@gmail.com>
2022-11-03 14:19:01 +0200

    Creating promise

--- index.js
+++ index.js
@@ -1,40 +1,28 @@
 
 /// Promise
 /*
+Створення промісу
 
-Pending - очікування
+const promise = new Promise(executor);
 
-Resolve - вирішений (успішний)
-Reject - відхилений (неуспішний)
-
+executor = function
 
 */
 
-const resultOfRequest = fetch('./data.json');
 
-resultOfRequest
-.then((responce)=>responce.json())
-.then((successData) => {
-    const card = createUserCard(successData);
-    const root = document.querySelector('#root');
-    root.append(card);
-})
-.catch((reject)=>{
-    console.log(reject);
+const promise = new Promise(function(resolve, reject){
+       /* if(it`s okay){
+            resolve()
+        } else {
+            reject()
+        }
+        */
 });
 
-function createUserCard(user) {
-    const h2 = createElement('h2', {classNames: ['username']}, user.firstName, user.lastName);
-    const p = createElement('p', {}, user.telNum);
-    const div = createElement('div', {classNames: ['card-wrapper']}, h2, p);
-    return div;
-}
+///////////////////
 
-
-
-function createElement(type, {classNames=[]}, ...children) {
-    const elem = document.createElement(type);
-    elem.classList.add(...classNames);
-    elem.append(...children);
-    return elem;
-}
+promise.then(()=>{
+    ///resolve callback
+},()=>{
+    //reject callback
+})
